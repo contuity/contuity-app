b437866e83ebca0c1d2bf5e4b64a1fdc4bd81ed0
Ryan Hughes <ryanhughes624@gmail.com>
2019-03-18 15:25:19 -0400

    minor change to shouldComponentUpdate

--- src/screens/AllJotsScreen.js
+++ src/screens/AllJotsScreen.js
@@ -49,20 +49,22 @@ class AllJotsScreen extends Component {
     }
 
     // do not re-render if when selecting multiple jots
-    if (this.state.listSelectionMode && nextState.listSelectionMode) {
-      if (
-        this.state.selectedJots.length === 0 &&
-        nextState.selectedJots.length !== 1
-      ) {
-        return false;
-      }
+    if (!this.state.listSelectionMode || !nextState.listSelectionMode) {
+      return true;
+    }
 
-      if (
-        this.state.selectedJots.length === 1 &&
-        nextState.selectedJots.length !== 0
-      ) {
-        return false;
-      }
+    if (
+      this.state.selectedJots.length === 0 &&
+      nextState.selectedJots.length !== 1
+    ) {
+      return false;
+    }
+
+    if (
+      this.state.selectedJots.length === 1 &&
+      nextState.selectedJots.length !== 0
+    ) {
+      return false;
     }
 
     return true;
