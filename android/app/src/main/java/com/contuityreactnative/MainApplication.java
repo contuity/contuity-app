59d7aaef4af372917db2eb74eec6b82a240bbdfe
Ryan Hughes <ryanhughes624@gmail.com>
2019-03-28 16:49:00 -0400

    i deleted some folders and ran react-native eject

--- android/app/src/main/java/com/contuityreactnative/MainApplication.java
+++ android/app/src/main/java/com/contuityreactnative/MainApplication.java
@@ -3,8 +3,6 @@ package com.contuityreactnative;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
-import com.oblador.vectoricons.VectorIconsPackage;
-import io.realm.react.RealmReactPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
@@ -24,9 +22,7 @@ public class MainApplication extends Application implements ReactApplication {
     @Override
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
-          new MainReactPackage(),
-            new VectorIconsPackage(),
-            new RealmReactPackage()
+          new MainReactPackage()
       );
     }
 
