19d247b5431e7468634edca4906568fc3c7b9f3c
Caitlin Wang <caitlinwang97@gmail.com>
2019-04-01 19:47:55 -0400

    restore App.js

--- App.js
+++ App.js
@@ -21,13 +21,13 @@ class App extends Component {
   }
 
   render() {
-    // if (this.state.user == null) {
-    //   return <Login onLogin={this.onLogin} />;
-    // } else {
-    //   return <AllJotsScreen user={this.state.user} />;
-    // }
-    // return <AllJotsScreen user={this.state.user} />;
-    return <PeopleScreen />;
+    if (this.state.user == null) {
+      return <Login onLogin={this.onLogin} />;
+    } else {
+      return <AllJotsScreen user={this.state.user} />;
+    }
+    return <AllJotsScreen user={this.state.user} />;
+    // return <PeopleScreen />;
   }
 }
 
