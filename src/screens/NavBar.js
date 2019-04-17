1a3e73642144de00f61e1ed3a5f6865cdba079d6
Ryan Hughes <ryanhughes624@gmail.com>
2019-04-16 22:45:09 -0400

    yarn rfix

--- src/screens/NavBar.js
+++ src/screens/NavBar.js
@@ -3,28 +3,27 @@ import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import AllJotsScreen from './AllJotsScreen';
 import PeopleScreen from './PeopleScreen';
 
-import jotUnselectedIcon from '../resources/jots.png'
-import spacesUnselectedIcon from '../resources/spaces.png'
-import peopleUnselectedIcon from '../resources/people.png'
+import jotUnselectedIcon from '../resources/jots.png';
+import spacesUnselectedIcon from '../resources/spaces.png';
+import peopleUnselectedIcon from '../resources/people.png';
 
 // TODO: update these with the correct images
-import jotSelectedIcon from '../resources/jots.png'
-import spacesSelectedIcon from '../resources/spaces.png'
-import peopleSelectedIcon from '../resources/people.png'
-
+import jotSelectedIcon from '../resources/jots.png';
+import spacesSelectedIcon from '../resources/spaces.png';
+import peopleSelectedIcon from '../resources/people.png';
 
 let showingPage = {
   JOTS: 'jots',
   SPACES: 'spaces',
-  PEOPLE: 'people'
-}
+  PEOPLE: 'people',
+};
 
 class NavBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      showingPage: showingPage.JOTS
+      showingPage: showingPage.JOTS,
     };
 
     this.jotSelected = this.jotSelected.bind(this);
@@ -34,138 +33,119 @@ class NavBar extends Component {
 
   jotSelected() {
     this.setState({
-      showingPage: showingPage.JOTS
-    })
+      showingPage: showingPage.JOTS,
+    });
   }
 
   spacesSelected() {
     this.setState({
-      showingPage: showingPage.SPACES
-    })
+      showingPage: showingPage.SPACES,
+    });
   }
 
   peopleSelected() {
     this.setState({
-      showingPage: showingPage.PEOPLE
-    })
+      showingPage: showingPage.PEOPLE,
+    });
   }
 
-
   getButton(src, text, onPress) {
-
-    
     let leftButton = {
       flex: 1,
       flexDirection: 'row',
       padding: 'auto',
       width: '100%',
-      justifyContent: "center",
-      alignItems: "center",
-    }
+      justifyContent: 'center',
+      alignItems: 'center',
+    };
 
     let imageStyle = {
       margin: 'auto',
-    }
+    };
 
     let middleContainer = {
-
-      justifyContent: "center",
-      alignItems: "center",
-    }
-
+      justifyContent: 'center',
+      alignItems: 'center',
+    };
 
     let iconSubtitleText = {
       color: 'white',
       fontFamily: 'Assistant',
       fontSize: 14,
       lineHeight: 21,
-      justifyContent: "center",
-      alignItems: "center",
-    }
-
+      justifyContent: 'center',
+      alignItems: 'center',
+    };
 
     return (
       <TouchableOpacity style={leftButton} onPress={onPress}>
         <View style={middleContainer}>
-          <Image source={src} style={imageStyle}/>
+          <Image source={src} style={imageStyle} />
           <Text style={iconSubtitleText}>{text}</Text>
         </View>
-      </TouchableOpacity>)
-
+      </TouchableOpacity>
+    );
   }
 
   render() {
-
     let viewStyle = {
       flex: 1,
       flexDirection: 'column',
       height: '100%',
-      width: '100%'
-    }
-
+      width: '100%',
+    };
 
     let navBarStyle = {
       height: 72,
       flexDirection: 'row',
       width: '100%',
-      backgroundColor: "rgba(54, 50, 60, 0.82)",
+      backgroundColor: 'rgba(54, 50, 60, 0.82)',
       backdropFilter: 'blur(4px)',
-      zIndex: -1
+      zIndex: -1,
     };
 
     let contentView = {
       height: '90%',
       position: 'relative',
-      flex: 1
-    }
+      flex: 1,
+    };
 
     let currentPage = null;
 
-
-    // Figure out which icons to use and which page to show. 
+    // Figure out which icons to use and which page to show.
     let jotIcon;
     if (this.state.showingPage == showingPage.JOTS) {
-      jotIcon = jotSelectedIcon
-      currentPage = <AllJotsScreen user={this.state.user} />
+      jotIcon = jotSelectedIcon;
+      currentPage = <AllJotsScreen user={this.state.user} />;
+    } else {
+      jotIcon = jotUnselectedIcon;
     }
-    else {
-      jotIcon = jotUnselectedIcon
-    }
-
 
     let spacesIcon;
     if (this.state.showingPage == showingPage.SPACES) {
-      spacesIcon = spacesSelectedIcon
+      spacesIcon = spacesSelectedIcon;
+    } else {
+      spacesIcon = spacesUnselectedIcon;
     }
-    else {
-      spacesIcon = spacesUnselectedIcon
-    }
-
 
     let peopleIcon;
     if (this.state.showingPage == showingPage.PEOPLE) {
-      peopleIcon = peopleSelectedIcon
-      currentPage = <PeopleScreen/>;
+      peopleIcon = peopleSelectedIcon;
+      currentPage = <PeopleScreen />;
+    } else {
+      peopleIcon = peopleUnselectedIcon;
     }
-    else {
-      peopleIcon = peopleUnselectedIcon
-    }
-
 
     return (
       <View style={viewStyle}>
-          <View style={contentView}>
-            {currentPage}
-          </View>
-          <View style={navBarStyle}>
-
-            {this.getButton(jotIcon, 'Jots', this.jotSelected)}
-            {this.getButton(spacesIcon, 'Spaces', this.spacesSelected)}
-            {this.getButton(peopleIcon, 'People', this.peopleSelected)}
-            
-          </View>
+        <View style={contentView}>{currentPage}</View>
+        <View style={navBarStyle}>
+          {this.getButton(jotIcon, 'Jots', this.jotSelected)}
+          {this.getButton(spacesIcon, 'Spaces', this.spacesSelected)}
+          {this.getButton(peopleIcon, 'People', this.peopleSelected)}
+        </View>
       </View>
-      );
+    );
   }
 }
 
